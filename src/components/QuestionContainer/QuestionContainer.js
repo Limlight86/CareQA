01df3992a513014d50ce22bbfed35fd4b5ebeed4
Lazaro <lquin008@gmail.com>
2019-08-01 08:50:34 -0400

    set up confirmation

--- src/components/QuestionContainer/QuestionContainer.js
+++ src/components/QuestionContainer/QuestionContainer.js
@@ -1,30 +1,26 @@
-import React, { useContext } from "react"
-import { QAContext } from "../../context/QAcontext";
+import React, { useState } from "react"
 import Confirmation from '../Confirmation'
 import styles from "./QuestionContainer.module.css";
 
-const QuestionContainer = ({ question, answer, id, showAnswer, confirmation }) => {
-  const { questions, setQuestions } = useContext(QAContext)
+const QuestionContainer = ({ question, answer, id, showAnswer}) => {
+  const [showConfirmation, setShowConfirmation] = useState(false)
 
-  const handleClick = (id) =>{
-    const newQuestions = questions;
-    const index = newQuestions.findIndex(e => e.id === id);
-    newQuestions[index].confirmation = true;
-    setQuestions([...newQuestions])
+  const handleClick = () =>{
+    setShowConfirmation(true)
   }
 
   const containerStyle = [styles.qContainer, 'container']
 
   return (
     <div className={containerStyle.join(" ")}>
-      {confirmation ? 
+      {showConfirmation ? 
         <Confirmation 
           message={"Reveal the Answer?"} 
           type={"message"}
           id={id}/> : null
       }
       <p>{question}</p>
-      <button className="btn btn-primary" disabled={showAnswer} onClick={()=>handleClick(id)}>
+      <button className="btn btn-primary" disabled={false} onClick={handleClick}>
          {showAnswer ? "The Answer is..." : "Show Answer"}
       </button>
       {showAnswer ? <p className={styles.answer}>{answer}</p> : null}
--- src/context/QAcontext.js
+++ src/context/QAcontext.js
@@ -8,22 +8,16 @@ const QAContextProvider = ({ children }) => {
         id: 1,
         question: "What is the average the airspeed velocity of a (European) unladen swallow?",
         answer: "11 meters per second",
-        showAnswer: false,
-        confirmation: false
       },
       {
         id: 2,
         question: "What job does Laz want?",
         answer: "CareCloud Associate React Dev",
-        showAnswer: false,
-        confirmation: false
       },
       {
         id: 3,
         question: "Why should we hire him?",
         answer: "He will commit 110% to mastering the CareCloud UI tech stack, I am worth the investment!",
-        showAnswer: false,
-        confirmation: false
       }
     ])
 
